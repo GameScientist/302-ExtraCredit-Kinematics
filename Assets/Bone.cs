f50c41ddcffbd05146c3996b8a63dc831a4e7473
Tyler Kortge <tylerkortge@charter.net>
2021-03-13 15:51:36 -0500

    Sprouted multiple bones in chains from the first bone.

--- Assets/Bone.cs
+++ Assets/Bone.cs
@@ -4,54 +4,65 @@ using UnityEngine;
 
 public class Bone : MonoBehaviour
 {
-    Vector3 worldStart;
-    Vector3 worldEnd;
-    Bone parent;
-    float worldDir;
-    float dir;
-    float wiggleOffset;
-    float wiggleAmp;
-    float wiggleTimeScale;
-    float mag;
+    public LineRenderer line;
+    public float dir;
+    public Vector3 worldStart;
+    public Vector3 worldEnd;
+    public float worldDir;
+    public Bone parent;
+    public float wiggleOffset;
+    public float wiggleTimeScale;
+    public float wiggleAmp;
+    public float mag;
+    public int chainLength;
+    public bool bonesAdded = false;
 
-    private void Start()
+    // Start is called before the first frame update
+    void Start()
     {
-        parent = GetComponentInParent<Bone>();
-        dir = Random.Range(-1, 1);
+        line = GetComponentInChildren<LineRenderer>();
+        dir = Random.Range(-1, 2);
         wiggleOffset = Random.Range(0, 6.28f);
         wiggleAmp = Random.Range(.5f, 2);
         wiggleTimeScale = Random.Range(.25f, 1);
-        mag = Random.Range(0.5f, 2.5f);
+        mag = Random.Range(1f, 2f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        Calc();
-        LineRenderer line = GetComponent<LineRenderer>();
-        line.SetPosition(0, new Vector3(worldStart.x, worldStart.y, worldStart.z));
-        line.SetPosition(1, new Vector3(worldEnd.x, worldEnd.y, worldEnd.z));
-    }
-
-    private void Calc()
-    {
-        if (transform.parent != null)
+        if (parent != null)
         {
             worldStart = parent.worldEnd;
             worldDir = parent.worldDir + dir;
         }
         else
         {
-
-            worldStart = new Vector3(1, 1, 1);
+            worldStart = new Vector3(0, 0, 0);
             worldDir = dir;
+            chainLength = 5;
         }
-
         worldDir += Mathf.Sin((Time.time + wiggleOffset) * wiggleTimeScale) * wiggleAmp;
 
         Vector3 localEnd = new Vector3(mag * Mathf.Cos(worldDir), mag * Mathf.Sin(worldDir));
         localEnd *= mag;
 
         worldEnd = worldStart + localEnd;
+
+        line.SetPosition(0, new Vector3(worldStart.x, worldStart.y, worldStart.z));
+        line.SetPosition(1, new Vector3(worldEnd.x, worldEnd.y, worldEnd.z));
+
+        if (chainLength > 1 && bonesAdded == false)
+        {
+            int numOfChildren = (int)Random.Range(1, 4);
+
+            for (int i = 0; i < numOfChildren; i++)
+            {
+                Bone newBone = Instantiate(gameObject.GetComponent<Bone>());
+                newBone.parent = this;
+                newBone.chainLength = chainLength - 1;
+            }
+            bonesAdded = true;
+        }
     }
 }
